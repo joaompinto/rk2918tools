cbf38748b5253658c8f657f2f9d2e74ecf37f6fd
João Pinto <lamego.pinto@gmail.com>
2012-02-26 19:48:48 +0000

    Added *bootimg.pl from https://github.com/wesgarner/bin

--- device_info.py
+++ device_info.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python
 from commands import getoutput
-data = getoutput("./rkflashtool r 0x0 1024 2>/dev/null")
-device_info = data[:1024].strip('\x00').splitlines()
-for line in device_info:
-		print line
+import struct
+
+data = getoutput("./rkflashtool r 0x0 1 2>/dev/null")
+header = data[:4]
+if header != "PARM":
+	print "NAND content does not start with a parameters file!"
+	sys.exit()
+params_size = struct.unpack('<I', data[4:8])
+params_size = params_size[0]
+print "Device parameters:"
+print data[8:8+params_size]
+
+next_piece=data[8+params_size:8+params_size+4]
+params_size = struct.unpack('<I', next_piece)
+
+print "Next piece", next_piece
